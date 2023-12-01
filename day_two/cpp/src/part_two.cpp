a83ff14994f70f1cedce8f848ad6b384f0bfca66
Eshan Chatterjee <eshan2001221@gmail.com>
2023-12-02 01:15:05 +0530

    Add day 2 stub for CPP

---
+++ day_two/cpp/src/part_two.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <vector>
+
+
+
+auto main(int argc, char* argv[]) -> int {
+
+    std::cout << "To be continued...";
+    return 0;
+}
