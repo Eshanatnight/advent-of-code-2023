3c4998bd5fe7d20e06246679f214ef5b3fe7d264
Eshan Chatterjee <eshan2001221@gmail.com>
2023-12-06 22:38:29 +0530

    Day 6 Done!!

--- day_six/rust/src/bin/part_one.rs
+++ day_six/rust/src/bin/part_one.rs
@@ -1,3 +1,77 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Game {
+    races: HashMap<usize, HashMap<usize, usize>>
+}
+
+impl Game {
+    fn parse(input: &str) -> Self {
+        let mut game = Game {
+            races: HashMap::new()
+        };
+
+        let game_split = input.split("\n").collect::<Vec<_>>();
+
+        let times = parse_numbers(game_split[0].replace("Time: ", "").as_str());
+        let distances = parse_numbers(game_split[1].replace("Distance: ", "").as_str());
+
+        for (index, race_length) in times.iter().enumerate() {
+            let mut times: HashMap<usize, usize> = HashMap::new();
+            let distance_record = distances[index];
+
+
+            for hold_length in 1..race_length - 1 {
+                let distance = (race_length - hold_length) * hold_length;
+
+                if distance > distance_record {
+                    times.insert(hold_length, distance);
+            }
+        }
+
+        game.races.insert(*race_length, times);
+    }
+        game
+    
+}
+
+}
+
+fn parse_numbers(input: &str) -> Vec<usize> {
+    let mut numbers = Vec::new();
+
+    let mut in_number = false;
+    let mut number = String::new();
+
+    for c in input.chars() {
+        if c.is_digit(10) {
+            in_number = true;
+            number.push(c);
+        } else if in_number {
+            numbers.push(number.parse::<usize>().unwrap());         // directly unwrap here, because we know it's a number
+            number = String::new();
+            in_number = false;
+        }
+    }
+
+    if in_number {
+        numbers.push(number.parse::<usize>().unwrap());
+    }
+
+    numbers
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let input = include_str!("../../part_one.sample.txt");
+    let input = include_str!("../../part_one.input.txt");
+
+    let game = Game::parse(input);
+
+    let res: usize = game.races
+        .values()
+        .map(|v| v.len())
+        .product();
+
+
+    println!("Result: {}", res)
 }
--- day_six/rust/src/bin/part_two.rs
+++ day_six/rust/src/bin/part_two.rs
@@ -1,3 +1,62 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Game {
+    races: HashMap<usize, HashMap<usize, usize>>
+}
+
+impl Game {
+    fn parse(input: &str) -> Self {
+        let mut game = Game {
+            races: HashMap::new()
+        };
+
+        let game_split = input.split("\n").collect::<Vec<_>>();
+
+        let times = vec![game_split[0]
+            .replace("Time: ", "")
+            .replace(" ", "")
+            .parse::<usize>()
+            .unwrap()];
+
+        let distances = vec![game_split[1]
+            .replace("Distance: ", "")
+            .replace(" ", "")
+            .parse::<usize>()
+            .unwrap()];
+
+        for (index, race_length) in times.iter().enumerate() {
+            let mut times: HashMap<usize, usize> = HashMap::new();
+            let distance_record = distances[index];
+
+
+            for hold_length in 1..race_length - 1 {
+                let distance = (race_length - hold_length) * hold_length;
+
+                if distance > distance_record {
+                    times.insert(hold_length, distance);
+            }
+        }
+
+        game.races.insert(*race_length, times);
+    }
+        game
+    
+    }
+
+}
+
 fn main() {
-    println!("Part One")
+    // let input = include_str!("../../part_two.sample.txt");
+    let input = include_str!("../../part_two.input.txt");
+
+    let game = Game::parse(input);
+
+    let res: usize = game.races
+        .values()
+        .map(|v| v.len())
+        .product();
+
+
+    println!("Result: {}", res)
 }
