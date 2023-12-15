fad423efdf142ef5cfa9332f85785c2e543e68b7
Eshan Chatterjee <eshan2001221@gmail.com>
2023-12-16 00:01:54 +0530

    Day 15 Done Lesss Gooo

---
+++ Day-15 Lens Library/premake5.lua
@@ -0,0 +1,24 @@
+workspace "Lens Library"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+
+local outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "part_one"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir ("build/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("build/bin-int/" .. outputdir .. "/%{prj.name}")
+    includedirs { "../cpputils/" }
+    files { "src/lens_library_part_one.cpp", "input.txt", "sample.txt"}
+
+
+project "part_two"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir ("build/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("build/bin-int/" .. outputdir .. "/%{prj.name}")
+    includedirs { "../cpputils/" }
+    files { "src/lens_library_part_two.cpp", "input.txt", "sample.txt" }
---
+++ Day-15 Lens Library/src/lens_library_part_one.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+[[nodiscard]] auto to_string_vec(std::ifstream& fin) -> std::vector<std::string> {
+	std::vector<std::string> vec;
+	std::string line;
+	while (std::getline(fin, line)) {
+		vec.push_back(line);
+	}
+	return vec;
+}
+
+[[nodiscard]] auto split(const std::string& str, char delimeter = ' ') -> std::vector<std::string> {
+	std::vector<std::string> vec;
+	std::stringstream ss(str);
+	std::string token;
+	while (std::getline(ss, token, delimeter)) {
+		vec.push_back(token);
+	}
+	return vec;
+}
+
+[[nodiscard]] auto calculate_hash(const std::string& hash) -> uint64_t {
+    uint64_t result{0};
+
+    for(int i = 0; i < hash.size(); ++i) {
+        result += static_cast<uint64_t>(hash[i]);
+        result *= 17;
+        result %= 256;
+    }
+
+    return result;
+
+}
+
+auto main(int argc, char* argv[]) -> int {
+    if(argc <= 1) {
+        std::cerr << "Invalid Useage\n";
+        return 1;
+    }
+
+    std::string file_name;
+    if(*argv[1] == '1') {
+        file_name = "sample.txt";
+    } else {
+        file_name = "input.txt";
+    }
+    
+    std::ifstream fin(file_name);
+    if(!fin) {
+        std::cerr << "Could not open file: " << file_name << std::endl;
+        return 1;
+    }
+
+    auto vec = to_string_vec(fin);
+    std::vector<std::string> hashes = split(vec[0], ',');
+    
+    uint64_t val{0};
+
+    for (const auto& hash : hashes) {
+        val += calculate_hash(hash);
+    }
+
+    std::cout << "Solution: " << val << '\n';
+
+    return 0;
+}
